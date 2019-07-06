f896f4c0784058962aeff1008ed940c8db2843fc
Chad Wade Day, Jr <cdayjr@users.noreply.github.com>
2019-07-06 16:00:17 -0700

    First inquirer question up and working.

--- src/install.ts
+++ src/install.ts
@@ -1,6 +1,12 @@
 import yargs from "yargs";
 import process from "process";
+import inquirer, { InputQuestion, NumberQuestion } from "inquirer";
 
+/**
+ * Config interface
+ *
+ * What the end result config should look like type-wise
+ */
 interface Config {
   boardname: string;
   domain: string;
@@ -17,82 +23,130 @@ interface Config {
   sql_prefix: string;
 }
 
-const options = yargs
-  .scriptName("jaxboards-install")
-  .usage("$0 [args]")
-  .command("$0", "Install JaxBoards databae and print configuration.")
-  .option("configOnly", {
-    description:
-      "Only generate config- do not connect to or install the database",
-    type: "boolean",
-    default: true // will change when database install is implemented
-  })
-  .option("boardname", {
-    description: "The board name",
-    default: "Jaxboards"
-  })
-  .option("domain", {
-    description: "The domain the board will run on",
-    default: "example.com"
-  })
-  .option("mail_from", {
-    description: "The address email will be sent from",
-    default: "Example <example@example.com>"
-  })
-  .option("sql_db", {
-    description: "The database the board will be installed to",
-    default: "jaxboards"
-  })
-  .option("sql_host", {
-    description: "The database host the board will be installed to",
-    default: "localhost"
-  })
-  .option("sql_port", {
-    description: "The database host port the board will be installed to",
-    default: 3306,
-    type: "number"
-  })
-  .option("sql_username", {
-    description: "The database username",
-    default: "jaxboards"
-  })
-  .option("sql_password", {
-    description: "The database password",
-    default: "jaxboards"
-  })
-  .option("sql_prefix", {
-    description:
-      "The database table prefix, for installing multiple instances of the board to the same database",
-    default: ""
-  })
-  .options("config", {
-    description: "Provide a path to an existing configuration file."
-  }).argv;
-
-const config: Config = {
-  boardname: options.boardname,
-  domain: options.domain,
-  // eslint-disable-next-line @typescript-eslint/camelcase
-  mail_from: options.mail_from,
-  // eslint-disable-next-line @typescript-eslint/camelcase
-  sql_host: options.sql_host,
-  // eslint-disable-next-line @typescript-eslint/camelcase
-  sql_port: options.sql_port || 3306,
-  // eslint-disable-next-line @typescript-eslint/camelcase
-  sql_db: options.sql_db,
-  // eslint-disable-next-line @typescript-eslint/camelcase
-  sql_username: options.sql_username,
-  // eslint-disable-next-line @typescript-eslint/camelcase
-  sql_password: options.sql_password,
-  // eslint-disable-next-line @typescript-eslint/camelcase
-  sql_prefix: options.sql_prefix,
-  installed: true,
-  service: false,
-  prefix: ""
-};
-
-if (!options.configOnly) {
-  process.stderr.write(`Database install not implemented.\n`);
+/**
+ * Answers interface
+ *
+ * The results of the inquirer answers
+ */
+interface Answers {
+  boardname?: string;
+  domain?: string;
+  mail_from?: string;
+  sql_host?: string;
+  sql_port?: number;
+  sql_db?: string;
+  sql_username?: string;
+  sql_password?: string;
+  sql_driver?: string;
+  installed?: boolean;
+  service?: boolean;
+  prefix?: string;
+  sql_prefix?: string;
 }
 
-process.stdout.write(`${JSON.stringify(config, null, 2)}\n`);
+const main = async (): Promise<void> => {
+  // This is where the command line arguments are set
+  const options = yargs
+    .scriptName("jaxboards-install")
+    .usage("$0 [args]")
+    .command("$0", "Install JaxBoards databae and print configuration.")
+    .option("configOnly", {
+      description:
+        "Only generate config- do not connect to or install the database",
+      type: "boolean",
+      default: true // will change when database install is implemented
+    })
+    .option("boardname", {
+      description: "The board name",
+      default: false
+    })
+    .option("domain", {
+      description: "The domain the board will run on",
+      default: false
+    })
+    .option("mail_from", {
+      description: "The address email will be sent from",
+      default: false
+    })
+    .option("sql_host", {
+      description: "The database host the board will be installed to",
+      default: false
+    })
+    .option("sql_port", {
+      description: "The database host port the board will be installed to",
+      default: false,
+      type: "number"
+    })
+    .option("sql_db", {
+      description: "The database the board will be installed to",
+      default: false
+    })
+    .option("sql_username", {
+      description: "The database username",
+      default: false
+    })
+    .option("sql_password", {
+      description: "The database password",
+      default: false
+    })
+    .option("sql_prefix", {
+      description:
+        "The database table prefix, for installing multiple instances of the board to the same database",
+      default: false
+    })
+    .options("config", {
+      description: "Provide a path to an existing configuration file."
+    }).argv;
+
+  if (!options.configOnly) {
+    // Database install not ready yet!
+    process.stderr.write(`Database install not implemented.\n`);
+  }
+
+  // Set the configuration
+  const config: Config = {
+    boardname: "Jaxboards",
+    domain: "example.com",
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    mail_from: "Example <example@example.com>",
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    sql_host: "localhost",
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    sql_port: 3306,
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    sql_db: "jaxboards",
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    sql_username: "jaxboards",
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    sql_password: "jaxboards",
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    sql_prefix: "",
+    installed: true,
+    service: false,
+    prefix: ""
+  };
+
+  // Interactive prompt for information we haven't gathered
+  const questionsToAsk: (InputQuestion<string> | NumberQuestion<number>)[] = [];
+
+  if (!options.boardname) {
+    questionsToAsk.push({
+      type: "input",
+      name: "boardname",
+      message: "What will this board's name be?",
+      default: config.boardname
+    });
+  }
+
+  if (questionsToAsk.length > 0) {
+    const answers: Answers = await inquirer.prompt(questionsToAsk);
+
+    if (undefined !== answers.boardname && answers.boardname) {
+      config.boardname = answers.boardname;
+    }
+  }
+
+  // Print the configuration
+  process.stdout.write(`${JSON.stringify(config, null, 2)}\n`);
+};
+main();
